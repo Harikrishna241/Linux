2fb8a51725fd2b8cfc6dbd1c1f90343c56be6a89
hkaranam <hkaranam241@gmail.com>
2024-08-17 15:15:01 -0700

    second commit

--- 06-install-packages.sh
+++ 06-install-packages.sh
@@ -12,9 +12,20 @@ else
 
 fi
 
+validate(){
+    if [ "$1" -eq 0 ]
+    then 
+        echo "the status of $2 installation is sucess"
+    else
+        echo " the status of $2 installation is failure "
+        exit 1
+    fi
+}
+
 echo "install packages $@"
 for i in $@;
 do 
     dnf install $i -y 
+    validate $? $i 
 done
 
