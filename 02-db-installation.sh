b84df25fcf68a716be406ea8390dce72516f9f32
hkaranam <hkaranam241@gmail.com>
2024-08-17 11:02:47 -0700

    second commit

--- 02-db-installation.sh
+++ 02-db-installation.sh
@@ -5,6 +5,7 @@ USERID = $id
 if [ USERID -eq 0 ]
 then 
     echo " you are in super user"
+    exit 1 # it will exit if the user is not su
 else
     echo "you need to change it to su user name"
     sudo su 
