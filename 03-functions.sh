a38ca285205313e92257c334b49d689c8c14cb7c
hkaranam <hkaranam241@gmail.com>
2024-08-17 12:03:53 -0700

    second commit

--- 03-functions.sh
+++ 03-functions.sh
@@ -4,18 +4,17 @@ USERID=$(id -u)
 
 validate(){
     echo "the status of the script:$1"
-    echo "installation of sript $2"
+    echo " $2 "
 }
 
 if [ "$USERID" -eq 0 ]
 then 
     echo " you are in super user"
-     # it will exit if the user is not su
+    
 else
     echo "you need to change it to su user name"
     exit 1
-    # sudo su 
-    # exit
+
 fi
 
 dnf install mysql -y
