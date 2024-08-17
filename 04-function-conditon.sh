434c68c95c14a99a3af47947ebf87e8f0907ea96
hkaranam <hkaranam241@gmail.com>
2024-08-17 12:08:18 -0700

    second commit

---
+++ 04-function-conditon.sh
@@ -0,0 +1,31 @@
+#! /bin/bash 
+
+USERID=$(id -u) 
+
+validate(){
+    if [ $1 =eq 0 ]
+    then 
+        echo "the status of $2 installation is sucess"
+    echo " $2 "
+}
+
+if [ "$USERID" -eq 0 ]
+then 
+    echo " you are in super user"
+    
+else
+    echo "you need to change it to su user name"
+    exit 1
+
+fi
+
+dnf install mysql -y
+validate $? " mysql"
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "installation script is failure"
+#     exit 1
+# else 
+#     echo "installation is success"
+# fi
