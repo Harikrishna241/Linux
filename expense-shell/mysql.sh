6886030bbd965ed2a15e322dfb3980366e0a4514
hkaranam <hkaranam241@gmail.com>
2024-08-17 16:42:28 -0700

    second commit

---
+++ expense-shell/mysql.sh
@@ -0,0 +1,39 @@
+#! /bin/bash 
+
+#installation of my sql 
+USERID=$(id -u)
+
+
+if [ $USERID -eq 0 ]
+then 
+    echo "you are in super user mode and you can run the script"
+else 
+    echo "please run the script in su user"
+    exit 1
+fi
+# now i need to check whther the mysql-server install or not 
+yum list installed mysql-server
+if [ $? -eq 0]
+then 
+    echo "mysql-server package installed"
+    exit 1
+else
+    echo "installing the mysql-server"
+    dnf install mysql-server -y
+fi
+
+#enabling the systemctl services for mysql-server
+
+STATUS="$(systemctl is-active mysqld.service)"
+if [ "${STATUS}" = "active" ]; then
+    echo "mysqld your tasks ....."
+    exit 1
+else 
+    echo " Service not running.... so activating"  
+    systemctl enable mysqld
+    systemctl start mysqld 
+fi
+
+#Setting up the root password for the mysql server 
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
